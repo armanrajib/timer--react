ceb4b7bb216dc64126d87db29f070149a8097499
Arman Rajib <ahrajib6789@gmail.com>
2024-12-26 19:55:30 +0600

    Use forwardRef with typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
+import { useRef } from "react";
+
 import Button from "./components/Button";
 import Container from "./components/Container";
 import Input from "./components/Input";
 
 function App() {
+  const inputNameRef = useRef<HTMLInputElement>(null);
+  const inputAgeRef = useRef<HTMLInputElement>(null);
+
   return (
     <main>
-      <Input id="name" label="Your name" type="text" />
-      <Input id="age" label="Your age" type="number" />
+      <Input id="name" label="Your name" type="text" ref={inputNameRef} />
+      <Input id="age" label="Your age" type="number" ref={inputAgeRef} />
 
       <div style={{ display: "flex", gap: "1rem" }}>
         <Button>Submit</Button>
