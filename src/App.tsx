8820e7ac6c2a888ddd1cf590d1abb6f9b2cd5b09
Arman Rajib <ahrajib6789@gmail.com>
2024-12-26 23:21:18 +0600

    Create a custom wrapper component 'Form' with 'unknown' type casting, and expose component APIs with useImperativeHandle with typescript

--- src/App.tsx
+++ src/App.tsx
@@ -2,27 +2,37 @@ import { useRef } from "react";
 
 import Button from "./components/Button";
 import Container from "./components/Container";
+import Form, { type FormHandle } from "./components/Form";
 import Input from "./components/Input";
 
 function App() {
   const inputNameRef = useRef<HTMLInputElement>(null);
   const inputAgeRef = useRef<HTMLInputElement>(null);
+  const customFormRef = useRef<FormHandle>(null);
+
+  function handleSave(data: unknown) {
+    const extractedData = data as { name: string; age: string };
+    console.log(extractedData);
+    customFormRef.current?.clear();
+  }
 
   return (
     <main>
-      <Input id="name" label="Your name" type="text" ref={inputNameRef} />
-      <Input id="age" label="Your age" type="number" ref={inputAgeRef} />
+      <Form onSave={handleSave} ref={customFormRef}>
+        <Input id="name" label="Your name" type="text" ref={inputNameRef} />
+        <Input id="age" label="Your age" type="number" ref={inputAgeRef} />
 
-      <div style={{ display: "flex", gap: "1rem" }}>
-        <Button>Submit</Button>
-        <Button href="https://reactjs.org">Learn more</Button>
-      </div>
+        <div style={{ display: "flex", gap: "1rem" }}>
+          <Button>Submit</Button>
+          <Button href="https://reactjs.org">Learn more</Button>
+        </div>
 
-      <div style={{ marginTop: "1rem" }}>
-        <Container as={Button} href="https://google.com">
-          Click me
-        </Container>
-      </div>
+        <div style={{ marginTop: "1rem" }}>
+          <Container as={Button} href="https://google.com">
+            Click me
+          </Container>
+        </div>
+      </Form>
     </main>
   );
 }
---
+++ src/components/Form.tsx
@@ -0,0 +1,46 @@
+import {
+  type FormEvent,
+  type ComponentPropsWithoutRef,
+  useRef,
+  useImperativeHandle,
+  forwardRef,
+} from "react";
+
+export type FormHandle = {
+  clear: () => void;
+};
+
+type FormProps = ComponentPropsWithoutRef<"form"> & {
+  onSave: (value: unknown) => void;
+};
+
+const Form = forwardRef<FormHandle, FormProps>(function Form({ onSave, children, ...otherProps }, ref) {
+  const formRef = useRef<HTMLFormElement>(null);
+
+  useImperativeHandle(ref, () => {
+    return {
+      clear() {
+        console.log("CLEARING FORM!");
+        formRef.current?.reset();
+      },
+    };
+  });
+
+  function handleSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    const formData = new FormData(event.currentTarget);
+    const data = Object.fromEntries(formData);
+    onSave(data);
+
+    formRef.current?.reset();
+  }
+
+  return (
+    <form onSubmit={handleSubmit} {...otherProps} ref={formRef}>
+      {children}
+    </form>
+  );
+});
+
+export default Form;
